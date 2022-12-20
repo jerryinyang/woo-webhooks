99edcff95edd1a504b4275dc39f876ed44868399
jerryTheCreator <jerprog0@gmail.com>
2022-12-20 13:24:37 +0100

    attempting postgres 2 : updated db_create.py file

--- db_create.py
+++ db_create.py
@@ -1,8 +1,2 @@
 from app import db, User, Log
-import click
-from flask.cli import with_appcontext
-
-@click.command(name='create_tables')
-@with_appcontext
-def create_tables():
-    db.create_all()
+db.create_all()
